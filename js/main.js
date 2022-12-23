1971145a5e8d1e75da12e1e8a4d58b60049def24
Anthony Hamilton <anthonynhamilton@gmail.com>
2022-12-23 15:28:54 -0600

    made it so no errors when running random artwork

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 //Example fetch using pokemonapi.co
+
+
 function getFetch(){
-  const choice = Math.floor(Math.random()*100000)
+  const choice = Math.floor(Math.random()*118808)
   console.log(choice)
-  const url = 'https://api.artic.edu/api/v1/artworks/' + choice
+  const url = `https://api.artic.edu/api/v1/artworks?page=${choice}&limit=1`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        let imgID = data.data.image_id
+        let imgID = data.data[0].image_id
         document.querySelector('img').src = `https://www.artic.edu/iiif/2/${imgID}/full/843,/0/default.jpg`
-        document.querySelector('h2').innerText = data.data.title
-        document.querySelector('#artist').innerText = data.data.artist_title
-        document.querySelector('#materials').innerText = data.data.medium_display
-        document.querySelector('#year').innerText = data.data.date_display
+        document.querySelector('h2').innerText = data.data[0].title
+        document.querySelector('#artist').innerText = data.data[0].artist_title
+        document.querySelector('#materials').innerText = data.data[0].medium_display
+        document.querySelector('#year').innerText = data.data[0].date_display
 
       })
       .catch(err => {
