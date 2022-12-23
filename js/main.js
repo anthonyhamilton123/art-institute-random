3eaa54503d1cfa9cac1c54ceb30299178a0780bd
Anthony Hamilton <anthonynhamilton@gmail.com>
2022-12-23 12:43:50 -0600

    added directories, started to work on generator

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//Example fetch using pokemonapi.co
+function getFetch(){
+  const choice = Math.floor(Math.random()*50000)
+  console.log(choice)
+  const url = 'https://api.artic.edu/api/v1/artworks/' + choice
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('img').src = `https://www.artic.edu/iiif/2/${data.data.image_id}/full/843,/0/default.jpg`
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+getFetch()
